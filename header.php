856e40bfc64e0d53d4bfbf9cceaccf5d98da1d02
Andre_gina <49435055+andre2gina@users.noreply.github.com>
2019-05-08 17:05:11 +0300

    Update header.php

--- header.php
+++ header.php
@@ -6,28 +6,31 @@
 	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu:300" rel="stylesheet"> 
+	<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet">
+
 	<?php wp_head(); ?>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	
+	<!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link href="<?php echo get_template_directory_uri(); ?>/style-header.css" rel="stylesheet">
 	<link href="<?php echo get_template_directory_uri(); ?>/style-footer.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+	<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">-->
 </head>
 <header>
 	<div class="container">
-		<div class="header">
-			<div id="title">
-				<h1>MINIMØ</h1>
-			</div>
+		<header class="hero"><h1 class="logo"><a href="index.html">MinimØ</a></h1>
 			<nav>
 				<ul>
-					<li><a href="lifestyle.html">LIFESTYLE</a></li>
-					<li><a href="photodiary.html">PHOTODIARY</a></li>
-					<li><a href="music.html">MUSIC</a></li>
-					<li><a href="travel.html">TRAVEL</a></li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-</header>
-<body>
+					<a class="end" href="travel.html">Travel</a>
+
+					<a href="music.html">Music</a>
+
+					<a href="photodiary.html">Photodiary</a>
+
+					<a href="lifestyle.html">Lifestyle</a></ul>
+				</nav>
+				<img src="images/header.jpg" alt="">	
+			</div>
+		</header>
+		<body>
+
 
